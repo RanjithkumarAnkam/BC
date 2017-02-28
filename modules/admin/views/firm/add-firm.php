bcea236b2dbaf9adc244fc083524869a76c6c225
Praveenom3 <praveen@optionmatrix.com>
2017-02-28 15:33:40 +0530

    new changes

---
+++ modules/admin/views/firm/add-firm.php
@@ -0,0 +1,20 @@
+
+<section class="page-content ng-scope" ng-view="">
+	<section class="page-content-inner ng-scope">
+
+		<div class="header" role="alert">
+
+			<strong><i class="fa fa-plus"></i><span class="margin-left-10">ADD
+					FIRM</span></strong>
+		</div>
+		<div class="col-md-10">
+			<ul class="list-unstyled breadcrumb breadcrumb-custom">
+				<li><a href="javascript: void(0);">Firm</a></li>
+				<li><span>Add Firm</span></li>
+
+			</ul>
+		</div>
+		
+ <?php echo $this->render('_form'); ?>
+ </section>
+</section>
